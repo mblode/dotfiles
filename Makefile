464c3c1e8ea2b0ed5df5f148938ed2ae9c29574f
Matthew Blode <m.blode@gmail.com>
2016-09-14 21:30:44 +1000

    Mac os

--- Makefile
+++ Makefile
@@ -6,8 +6,10 @@ install:
 	cp home/.hushlogin ~/
 	cp home/.gitconfig ~/
 	cp home/.gitignore ~/
+	cp home/.macos ~/
 
 update:
 	cp ~/.vimrc ./home
 	cp ~/.zshrc ./home
 	cp ~/.gitconfig ./home
+	cp ~/.macos ./home
--- zsh/slim.zsh
+++ zsh/slim.zsh
@@ -12,13 +12,7 @@ setopt extendedglob
 
 export CLICOLOR=1
 
-source $slim_path/keys.zsh
-source $slim_path/history.zsh
-source $slim_path/completion.zsh
 source $slim_path/aliases.zsh
-source $slim_path/correction.zsh
-source $slim_path/stack.zsh
-source $slim_path/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 if command -v fasd >/dev/null 2>&1; then
   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
