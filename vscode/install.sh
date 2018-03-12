871bd0bac28b273dca2764b9331f1fe908f3e2a9
mblode <m.blode@gmail.com>
2018-03-13 08:21:26 +1100

    install

--- vscode/install.sh
+++ vscode/install.sh
@@ -9,21 +9,4 @@ if test "$(which code)"; then
 	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
 	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
 	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
-
-	# from `code --list-extensions`
-	modules="
-mblode.twig-language-2
-shinnn.stylelint
-mrmlnc.vscode-scss
-robinbentley.sass-indented
-esbenp.prettier-vscode
-christian-kohler.path-intellisense
-mblode.one-dark-italic
-PKief.material-icon-theme
-formulahendry.auto-close-tag
-formulahendry.auto-rename-tag
-"
-	for module in $modules; do
-		code --install-extension "$module" || true
-	done
 fi
