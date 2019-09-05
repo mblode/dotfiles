2c6658d802efe48f8492b38212c15f37d420a826
Matthew Blode <m@blode.co>
2019-09-05 22:14:28 +1000

    Revert pure

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,10 +1,6 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($DOTFILES/functions $fpath)
-
-autoload -U $DOTFILES/functions/*(:t)
-
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
