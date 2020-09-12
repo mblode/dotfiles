534709637347a69837faae68b1bee8599d87c783
Matthew Blode <m@blode.co>
2020-09-12 13:24:40 +1000

    coc.vim

---
+++ coc/install.sh
@@ -0,0 +1,19 @@
+
+# Coc
+
+echo "Installing coc..."
+
+cd $HOME/.config
+
+if [[ ! -e "coc" ]]; then
+    mkdir "coc"
+fi
+
+if [[ ! -e "nvim" ]]; then
+    mkdir "nvim"
+fi
+
+cd coc
+
+ln -sf "$HOME/dotfiles/coc/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+ln -sf "$HOME/dotfiles/coc/ultisnips" "$HOME/.config/coc/"
