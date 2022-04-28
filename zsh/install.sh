a37c00c45adb681fc9dd5f0e164c8181247641f9
Matthew Blode <m@blode.co>
2022-04-28 21:35:48 +1000

    Add zsh install script

---
+++ zsh/install.sh
@@ -0,0 +1,9 @@
+cd ~;
+mkdir -p .zsh;
+cd .zsh;
+git clone https://github.com/Aloxaf/fzf-tab;
+git clone https://github.com/djui/alias-tips;
+git clone https://github.com/zsh-users/zsh-autosuggestions;
+git clone https://github.com/zsh-users/zsh-completions;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting;
+git clone https://github.com/zsh-users/zsh-history-substring-search;
